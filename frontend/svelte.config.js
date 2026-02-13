38637e3a549e1f89ee599df089d72642a03d0491
falcon <ajith@tech4goodcommunity.com>
2026-02-14 00:14:07 +0530

    Fix: Fixed Frontend Deployment Issue

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node'; // Keep only node
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
+    // adapter-node will produce the /build folder used in the Dockerfile
     adapter: adapter()
   }
 };
